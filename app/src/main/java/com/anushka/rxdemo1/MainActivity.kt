45d73acece77180d65487552553bd38c2f769a7a
snaqviApps <naqvi.sdsu@gmail.com>
2023-01-22 23:15:22 -0600

    moved reactive-handling from MainActivity.kt for abstraction

--- app/src/main/java/com/anushka/rxdemo1/MainActivity.kt
+++ app/src/main/java/com/anushka/rxdemo1/MainActivity.kt
@@ -1,52 +1,30 @@
 package com.anushka.rxdemo1
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.anushka.rxdemo1.databinding.ActivityMainBinding
+import com.anushka.rxdemo1.react.ReactiveSetup
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
 
-    private val greeting = "Hello From RxJava"              // interim-input-stream
-    lateinit var _firstObservable : Observable<String>
-    lateinit var observer : Observer<String>
-
-    lateinit var textView: TextView
+    private lateinit var viewBasedBinding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewBasedBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.tvGreeting)
+        setContentView(viewBasedBinding.root)
 
-        _firstObservable = Observable.just(greeting)
-        observer = setObserver()
-
-        _firstObservable.subscribe(observer)
-
+        setupObservers().doOnNext {
+            viewBasedBinding.tvGreeting.text = it
+        }.subscribe()
 
     }
 
-    private fun setObserver() = object: Observer<String> {
-
-        override fun onSubscribe(d: Disposable) {
-            Log.d(TAG, "onSubscribe called")
-        }
-
-        override fun onNext(t: String) {
-            Log.d(TAG, "onNext called")
-            textView.text = t
-        }
-
-        override fun onError(e: Throwable) {
-            Log.d(TAG, "onError called")
-        }
-
-        override fun onComplete() {
-            Log.d(TAG, "onComplete called")
-        }
+    private fun setupObservers() : Observable<String> {
+        val reactiveSetup = ReactiveSetup()
+        return reactiveSetup._firstObservable
     }
 
     companion object {
--- app/src/main/java/com/anushka/rxdemo1/react/ReactiveSetup.kt
+++ app/src/main/java/com/anushka/rxdemo1/react/ReactiveSetup.kt
@@ -1,6 +1,43 @@
 package com.anushka.rxdemo1.react
 
+import android.util.Log
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+
 class ReactiveSetup {
 
+        private val greeting = "Hello From Reactive class: ${ReactiveSetup::class.simpleName}"              // interim-input-stream
+        var _firstObservable : Observable<String>
+        var observer : Observer<String>
+        init {
+            _firstObservable = Observable.just(greeting)
+            observer = setObserver()
+            _firstObservable.subscribe(observer)
+        }
 
-}
+        companion object {
+            private fun setObserver() = object: Observer<String> {
+
+                override fun onSubscribe(d: Disposable) {
+                    Log.d(TAG, "onSubscribe called")
+                }
+
+                override fun onNext(t: String) {
+                    Log.d(TAG, "onNext called")
+                }
+
+                override fun onError(e: Throwable) {
+                    Log.d(TAG, "onError called")
+                }
+
+                override fun onComplete() {
+                    Log.d(TAG, "onComplete called")
+                }
+            }
+
+            val TAG: String = Companion::class.java.toString()
+
+        }
+    }
+
